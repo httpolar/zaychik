88e4ed89a01cf76f50e6a5ad62c7e8a6acca5722
Kirill M. <96192210+httport@users.noreply.github.com>
2022-12-07 00:32:08 +0400

    removed a comment that got here from docs

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use commands::get_commands;
 use config::AppConfig;
 use poise::serenity_prelude as serenity;
 
-pub struct Data {} // User data, which is stored and accessible in all command invocations
+pub struct Data {}
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
