1b33446b9d0a13421018341383715d92ece6f553
Kirill M. <96192210+httport@users.noreply.github.com>
2022-12-07 23:57:48 +0400

    use my discord user converter in avatar command too

--- src/commands/avatar.rs
+++ src/commands/avatar.rs
@@ -1,16 +1,19 @@
-use poise::serenity_prelude as serenity;
+use poise::serenity_prelude::{self as serenity, CacheHttp};
 
-use crate::{Context, Error};
+use crate::{utils::UserFromUserId, Context, Error};
 
 /// Shows your or someone else's avatar
 #[poise::command(slash_command, prefix_command)]
 pub async fn avatar(
     ctx: Context<'_>,
-    user: Option<serenity::User>,
-    hidden: Option<bool>,
+    #[description = "Some discord user you want to look up"] user: Option<serenity::UserId>,
+    #[description = "If you'd like to hide command output"] hidden: Option<bool>,
 ) -> Result<(), Error> {
     let hidden = hidden.unwrap_or_else(|| false);
-    let user = user.as_ref().unwrap_or_else(|| ctx.author());
+    let user = match user {
+        Some(user_id) => serenity::User::from_id(ctx.http(), user_id).await?,
+        _ => ctx.author().clone(),
+    };
 
     let avatar_url = user
         .avatar_url()
