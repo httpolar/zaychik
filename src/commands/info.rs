abc0f28b608daf99fda211af0dc6ea17b69e8ad0
Kirill M. <96192210+httport@users.noreply.github.com>
2022-12-07 23:57:06 +0400

    added userinfo command

---
+++ src/commands/info.rs
@@ -0,0 +1,45 @@
+use poise::serenity_prelude::{self as serenity, CacheHttp};
+
+use crate::{utils::UserFromUserId, Context, Error};
+
+/// Shows some base info about Discord users
+#[poise::command(slash_command, prefix_command)]
+pub async fn userinfo(
+    ctx: Context<'_>,
+    #[description = "Some discord user you want to look up"] user: Option<serenity::UserId>,
+    #[description = "If you'd like to hide command output"] hidden: Option<bool>,
+) -> Result<(), Error> {
+    let hidden = hidden.unwrap_or(false);
+    let user = match user {
+        Some(user_id) => serenity::User::from_id(ctx.http(), user_id).await?,
+        _ => ctx.author().clone(),
+    };
+
+    let avatar_url = user.face();
+    let registered_human_readable = format!("<t:{}:F>", user.created_at().timestamp());
+
+    ctx.send(|reply| {
+        reply.ephemeral(hidden).embed(|e| {
+            e.title(format!("Discord user {}", &user.name));
+            e.thumbnail(&avatar_url);
+            e.field("Discord Tag", format!("{}", &user.tag()), true);
+            e.field("ID", format!("{}", &user.id), true);
+            e.field("Avatar", format!("[Avatar URL]({})", &avatar_url), true);
+            e.field("Registered At", &registered_human_readable, true);
+
+            if let Some(ref public_flags) = user.public_flags {
+                e.field("Public Flags", format!("{}", public_flags.bits()), true);
+            }
+
+            if let Some(accent_colour) = user.accent_colour {
+                e.field("Accent Colour", format!("#{}", accent_colour.hex()), true);
+                e.color(accent_colour);
+            }
+
+            e
+        })
+    })
+    .await?;
+
+    Ok(())
+}
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,7 +1,11 @@
 pub mod avatar;
+pub mod info;
 pub mod ping;
 
-
 pub async fn get_commands() -> Vec<poise::Command<crate::Data, crate::Error>> {
-    vec![self::avatar::avatar(), self::ping::ping()]
+    vec![
+        self::avatar::avatar(),
+        self::ping::ping(),
+        self::info::userinfo(),
+    ]
 }
