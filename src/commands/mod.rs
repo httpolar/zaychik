0eb17e904bd29570c175093af48cd341f34f091f
Kirill M. <96192210+httport@users.noreply.github.com>
2022-12-08 14:13:46 +0400

    added random number generator command

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,9 +1,11 @@
-pub mod info;
+mod info;
+mod random;
 
 pub async fn get_commands() -> Vec<poise::Command<crate::Data, crate::Error>> {
     vec![
         self::info::avatar(),
         self::info::userinfo(),
         self::info::ping(),
+        self::random::random(),
     ]
 }
---
+++ src/commands/random.rs
@@ -0,0 +1,34 @@
+use crate::{Context, Error};
+use rand::Rng;
+
+// Generates a random integer
+#[poise::command(slash_command, prefix_command)]
+pub async fn random(
+    ctx: Context<'_>,
+    #[description = "Lower bound (inclusive)"] min: Option<i64>,
+    #[description = "Upper bound (inclusive)"] max: Option<i64>,
+    #[description = "If you'd like to hide command output"] hidden: Option<bool>,
+) -> Result<(), Error> {
+    let hidden = hidden.unwrap_or(false);
+
+    let min: i64 = min.unwrap_or(1);
+    let max: i64 = max.unwrap_or(100);
+
+    if min > max {
+        ctx.send(|reply| {
+            reply
+                .ephemeral(true)
+                .content("Minimum number can not be bigger than maximum!")
+        })
+        .await?;
+        return Ok(());
+    }
+
+    let the_number: i64 = rand::thread_rng().gen_range(min..=max);
+    let response = format!("The number is {the_number}");
+
+    ctx.send(|reply| reply.ephemeral(hidden).content(response))
+        .await?;
+
+    Ok(())
+}
