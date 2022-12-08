5d41b5618ae71cc08cc9e794bc44e8918fdd5c21
Kirill M. <96192210+httport@users.noreply.github.com>
2022-12-08 12:30:09 +0400

    moved all info commands in info module

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -2,6 +2,42 @@ use poise::serenity_prelude::{self as serenity, CacheHttp};
 
 use crate::{utils::UserFromUserId, Context, Error};
 
+
+/// Test if the bot is online
+#[poise::command(slash_command, prefix_command)]
+pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.send(|reply| reply.ephemeral(true).content("Pong!"))
+        .await?;
+
+    Ok(())
+}
+
+
+/// Shows your or someone else's avatar
+#[poise::command(slash_command, prefix_command)]
+pub async fn avatar(
+    ctx: Context<'_>,
+    #[description = "Some discord user you want to look up"] user: Option<serenity::UserId>,
+    #[description = "If you'd like to hide command output"] hidden: Option<bool>,
+) -> Result<(), Error> {
+    let hidden = hidden.unwrap_or_else(|| false);
+    let user = match user {
+        Some(user_id) => serenity::User::from_id(ctx.http(), user_id).await?,
+        _ => ctx.author().clone(),
+    };
+
+    let avatar_url = user
+        .avatar_url()
+        .unwrap_or_else(|| user.default_avatar_url());
+    let msg = format!("{}'s avatar: {avatar_url}", user.name);
+
+    ctx.send(|reply| reply.ephemeral(hidden).content(msg))
+        .await?;
+
+    Ok(())
+}
+
+
 /// Shows some base info about Discord users
 #[poise::command(slash_command, prefix_command)]
 pub async fn userinfo(
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,11 +1,9 @@
-pub mod avatar;
 pub mod info;
-pub mod ping;
 
 pub async fn get_commands() -> Vec<poise::Command<crate::Data, crate::Error>> {
     vec![
-        self::avatar::avatar(),
-        self::ping::ping(),
+        self::info::avatar(),
         self::info::userinfo(),
+        self::info::ping(),
     ]
 }
