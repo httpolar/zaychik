33536b43a42abda56f14c932c658b223fd7de86c
Kirill M. <96192210+httport@users.noreply.github.com>
2022-12-11 18:44:20 +0400

    added descriptions and fixed logic of embed hiding

--- src/commands/external_apis.rs
+++ src/commands/external_apis.rs
@@ -3,12 +3,19 @@ use reqwest;
 
 use crate::{structs::saucenao::SearchResponse, Context, Error};
 
-const SAUCENAO_SEARCH: &'static str = "https://saucenao.com/search.php?output_type=2&numres=10&dedupe=2";
-
+const SAUCENAO_SEARCH: &'static str =
+    "https://saucenao.com/search.php?output_type=2&numres=10&dedupe=2";
 
 /// Reverse image search via saucenao
 #[poise::command(slash_command)]
-pub async fn sauce(ctx: Context<'_>, image: Attachment, hidden: Option<bool>) -> Result<(), Error> {
+pub async fn sauce(
+    ctx: Context<'_>,
+    image: Attachment,
+    #[description = "If you want to see low similarity results"] show_low_similarity: Option<bool>,
+    #[description = "If you'd like to hide command output"] hidden: Option<bool>,
+) -> Result<(), Error> {
+    let show_low_similarity = show_low_similarity.unwrap_or(false);
+
     match hidden {
         Some(true) => ctx.defer_ephemeral().await?,
         _ => ctx.defer().await?,
@@ -17,15 +24,20 @@ pub async fn sauce(ctx: Context<'_>, image: Attachment, hidden: Option<bool>) ->
     let client = &ctx.data().reqwest;
     let api_key = &ctx.data().keys.saucenao;
 
-    let url = reqwest::Url::parse_with_params(SAUCENAO_SEARCH, &[
-        ("api_key", api_key),
-        ("url", &image.url)
-    ])?;
+    let url = reqwest::Url::parse_with_params(
+        SAUCENAO_SEARCH,
+        &[("api_key", api_key), ("url", &image.url)],
+    )?;
 
     let data: SearchResponse = client.get(url).send().await?.json().await?;
 
     let mut response = String::new();
     for (entry, idx) in data.results.iter().zip(0..) {
+        let is_credible = entry.is_credible().unwrap_or(false);
+        if show_low_similarity == false && is_credible == false {
+            continue;
+        }
+
         if entry.data.ext_urls.as_ref().is_none() {
             continue;
         }
@@ -34,15 +46,14 @@ pub async fn sauce(ctx: Context<'_>, image: Attachment, hidden: Option<bool>) ->
 
         let url = match entry.data.ext_urls.as_ref().unwrap().get(0) {
             Some(v) => {
-                let is_credible = entry.is_credible().unwrap_or(false);
                 let mut result = String::new();
-                if !is_credible && idx != 0 {
+                if !is_credible || idx != 0 {
                     result.push_str("<");
                 }
 
                 result.push_str(v);
 
-                if !is_credible && idx != 0 {
+                if !is_credible || idx != 0 {
                     result.push_str(">");
                 }
 
