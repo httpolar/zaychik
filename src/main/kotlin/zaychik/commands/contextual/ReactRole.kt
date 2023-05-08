6fc3624ea699aa9b1ef89e7801d67f06ec09f526
Kirill M <96192210+httpolar@users.noreply.github.com>
2023-05-08 21:03:29 +0400

    store the created reaction role

--- src/main/kotlin/zaychik/commands/contextual/ReactRole.kt
+++ src/main/kotlin/zaychik/commands/contextual/ReactRole.kt
@@ -4,12 +4,15 @@ import dev.kord.common.entity.Snowflake
 import dev.kord.common.entity.TextInputStyle
 import dev.kord.core.behavior.interaction.modal
 import dev.kord.core.behavior.interaction.respondEphemeral
-import dev.kord.core.entity.ReactionEmoji
 import dev.kord.core.event.interaction.GuildMessageCommandInteractionCreateEvent
 import dev.kord.core.event.interaction.GuildModalSubmitInteractionCreateEvent
+import dev.kord.core.exception.EntityNotFoundException
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.*
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import zaychik.db.entities.ReactRole
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.time.Duration.Companion.seconds
@@ -18,6 +21,8 @@ import kotlin.time.Duration.Companion.seconds
 suspend fun handleReactRoleCreateFromRightClick(event: GuildMessageCommandInteractionCreateEvent) {
     val kord = event.kord
     val actor = event.interaction.user
+    val guild = event.interaction.guild
+
     val originalMessage = event.interaction.messages.values.firstOrNull() ?:
     // tell user things may fuck up sometimes
     return
@@ -82,10 +87,22 @@ suspend fun handleReactRoleCreateFromRightClick(event: GuildMessageCommandIntera
         return
     }
 
+    val role = try {
+        guild.getRole(Snowflake(roleId)).fetchRole()
+    } catch (e: EntityNotFoundException) {
+        null
+    }
+
+    if (role == null) {
+        submission.interaction.respondEphemeral {
+            content = ":x: You have provided a non-existent role!"
+        }
+        return
+    }
+
     val emojiMatch = """<(?<animated>a)?:(?<name>[a-zA-Z\d_]+):(?<id>\d+)>""".toRegex().matchEntire(providedEmoji)
 
-    val isEmojiAnimated = emojiMatch?.groups?.get("animated") != null
-    val emojiName =  emojiMatch?.groups?.get("name")?.value
+    val emojiName = emojiMatch?.groups?.get("name")?.value
     val emojiId = emojiMatch?.groups?.get("id")?.value?.toLongOrNull()?.let { Snowflake(it) }
 
     if (emojiName == null || emojiId == null) {
@@ -95,8 +112,31 @@ suspend fun handleReactRoleCreateFromRightClick(event: GuildMessageCommandIntera
         return
     }
 
-    val emoji = ReactionEmoji.Custom(emojiId, emojiName, isEmojiAnimated)
+    val emoji = guild.emojis.buffer(Channel.UNLIMITED)
+        .filter { e -> e.name == emojiName && e.id == emojiId }
+        .take(1)
+        .firstOrNull()
+
+    if (emoji == null) {
+        submission.interaction.respondEphemeral {
+            content = ":x: You have provided a non-existent emoji!"
+        }
+        return
+    }
+
     originalMessage.addReaction(emoji)
+
+    newSuspendedTransaction(Dispatchers.IO) {
+        ReactRole.new {
+            this.guildId = guild.id.value.toLong()
+            this.channelId = originalMessage.channelId.value.toLong()
+            this.messageId = originalMessage.id.value.toLong()
+            this.roleId = role.id.value.toLong()
+            this.emojiId = emoji.id.value.toLong()
+            this.enabled = true
+
+        }
+    }
 
     submission.interaction.respondEphemeral {
         content = "<:verified:1061586076013170719> Done!"
