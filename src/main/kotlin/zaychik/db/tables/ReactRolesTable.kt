f86c112b65c412534330e60d23def0cc538484b9
Kirill M <96192210+httpolar@users.noreply.github.com>
2023-05-09 16:37:41 +0400

    guild_id cant be null and unique index is on emoji_id instead

--- src/main/kotlin/zaychik/db/tables/ReactRolesTable.kt
+++ src/main/kotlin/zaychik/db/tables/ReactRolesTable.kt
@@ -5,10 +5,10 @@ import org.jetbrains.exposed.dao.id.UUIDTable
 object ReactRolesTable : UUIDTable() {
     override val tableName: String = "react_roles"
 
-    val guildId = long("guild_id").nullable().index()
+    val guildId = long("guild_id").index()
     val channelId = long("channel_id")
-    val messageId = long("message_id").uniqueIndex()
+    val messageId = long("message_id").index()
     val roleId = long("role_id").nullable().index()
-    val emojiId = long("emoji_id").index()
+    val emojiId = long("emoji_id").uniqueIndex()
     val enabled = bool("enabled").default(true)
 }
