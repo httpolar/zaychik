aa06cfbaca0cff5f9fd0b11f402f65ddc441c6e7
Kirill M <96192210+httpolar@users.noreply.github.com>
2023-05-11 22:57:20 +0400

    added extension function to ReactRole that lets fetching ReactRole easier from reaction events

--- src/main/kotlin/zaychik/db/entities/ReactRole.kt
+++ src/main/kotlin/zaychik/db/entities/ReactRole.kt
@@ -1,8 +1,12 @@
 package zaychik.db.entities
 
+import dev.kord.core.entity.ReactionEmoji
+import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.dao.UUIDEntity
 import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import zaychik.db.tables.ReactRolesTable
 import java.util.UUID
 
@@ -15,4 +19,17 @@ class ReactRole(id: EntityID<UUID>) : UUIDEntity(id) {
     var roleId by ReactRolesTable.roleId
     var emojiId by ReactRolesTable.emojiId
     var enabled by ReactRolesTable.enabled
+}
+
+suspend fun ReactRole.Companion.fromReactionEmoji(reaction: ReactionEmoji?, messageId: Long): ReactRole? {
+    val emoji = if (reaction is ReactionEmoji.Custom) reaction else null ?: return null
+    val emojiId = emoji.id.value.toLong()
+
+    val reactRole = newSuspendedTransaction(Dispatchers.IO) {
+        ReactRole.find { (ReactRolesTable.messageId eq messageId) and (ReactRolesTable.emojiId eq emojiId) }
+            .limit(1)
+            .firstOrNull()
+    } ?: return null
+
+    return reactRole
 }
