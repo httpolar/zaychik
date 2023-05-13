d75914780731dd0e657312d62ebf9267352406c4
Kirill M <96192210+httpolar@users.noreply.github.com>
2023-05-13 15:28:22 +0400

    wrapped kotlinx packages in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ val mainPath = "zaychik.MainKt"
 val koinVersion = "3.4.0"
 val exposedVersion = "0.41.1"
 
+fun kotlinx(name: String, version: String) = "org.jetbrains.kotlinx:kotlinx-$name:$version"
+
 plugins {
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.serialization") version "1.8.21"
@@ -28,13 +30,13 @@ tasks.shadowJar {
 }
 
 dependencies {
-    implementation("io.github.oshai:kotlin-logging-jvm:4.0.0-beta-28")
-    implementation("ch.qos.logback:logback-classic:1.4.7")
-    implementation("org.jetbrains.kotlinx:kotlinx-cli-jvm:0.3.5")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-    implementation("dev.kord:kord-core:0.9.0")
+    implementation("io.github.oshai", "kotlin-logging-jvm", "4.0.0-beta-28")
+    implementation("ch.qos.logback", "logback-classic", "1.4.7")
+    implementation(kotlinx("cli-jvm", "0.3.5"))
+    implementation(kotlinx("datetime", "0.4.0"))
+    implementation(kotlinx("coroutines-core", "1.7.1"))
+    implementation(kotlinx("serialization-json", "1.5.1"))
+    implementation("dev.kord", "kord-core", "0.9.0")
     implementation("com.zaxxer", "HikariCP", "5.0.1")
     implementation("org.postgresql", "postgresql", "42.6.0")
     implementation("org.jetbrains.exposed", "exposed-core", exposedVersion)
