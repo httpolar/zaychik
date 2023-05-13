2c0ee0c7aef656b47bd679953e497da17eec2d0a
Kirill M <96192210+httpolar@users.noreply.github.com>
2023-05-13 14:53:49 +0400

    upgraded dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 val mainPath = "zaychik.MainKt"
 
 val koinVersion = "3.4.0"
-val exposedVersion = "0.40.1"
+val exposedVersion = "0.41.1"
 
 plugins {
     kotlin("jvm") version "1.8.21"
@@ -20,7 +20,7 @@ repositories {
 }
 
 kotlin {
-    jvmToolchain(19)
+    jvmToolchain(17)
 }
 
 tasks.shadowJar {
@@ -28,15 +28,15 @@ tasks.shadowJar {
 }
 
 dependencies {
-    implementation("io.github.oshai:kotlin-logging-jvm:4.0.0-beta-22")
+    implementation("io.github.oshai:kotlin-logging-jvm:4.0.0-beta-28")
     implementation("ch.qos.logback:logback-classic:1.4.7")
-    implementation("org.jetbrains.kotlinx:kotlinx-cli-jvm:0.3.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-cli-jvm:0.3.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
     implementation("dev.kord:kord-core:0.9.0")
     implementation("com.zaxxer", "HikariCP", "5.0.1")
-    implementation("org.postgresql", "postgresql", "42.3.1")
+    implementation("org.postgresql", "postgresql", "42.6.0")
     implementation("org.jetbrains.exposed", "exposed-core", exposedVersion)
     implementation("org.jetbrains.exposed", "exposed-dao", exposedVersion)
     implementation("org.jetbrains.exposed", "exposed-jdbc", exposedVersion)
