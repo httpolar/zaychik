56dde9a60a90bea8532464d1e34f98567a0a5cf3
Kirill M <96192210+httpolar@users.noreply.github.com>
2023-05-20 18:47:29 +0400

    added another parser for partial emojis

--- src/main/kotlin/zaychik/utils/Kord.kt
+++ src/main/kotlin/zaychik/utils/Kord.kt
@@ -11,3 +11,16 @@ fun partialEmoji(id: Snowflake, name: String = "x", isAnimated: Boolean = false)
 fun partialEmoji(id: ULong, name: String = "x", isAnimated: Boolean = false): DiscordPartialEmoji {
     return partialEmoji(Snowflake(id), name, isAnimated)
 }
+
+fun partialEmoji(emoji: String): DiscordPartialEmoji {
+    val match = """<(?<isAnimated>a?):(?<name>[a-zA-Z_\-\d]+):(?<id>\d+)>""".toRegex().matchEntire(emoji)
+        ?: error("invalid custom emoji string")
+
+    val id = match.groups["id"]?.value?.toULongOrNull()
+        ?: error("couldn't parse emoji id which is required to build a partial emoji")
+
+    val name = match.groups["name"]?.value ?: "emoji"
+    val isAnimated = match.groups["isAnimated"]?.value == "a"
+
+    return partialEmoji(id, name, isAnimated)
+}
