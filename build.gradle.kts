b4a01f1732dd5c8373e84a43aa005a216c591763
Kirill M <96192210+httpolar@users.noreply.github.com>
2023-06-23 01:57:40 +0400

    kotlin logger updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ kotlin {
 }
 
 dependencies {
-    implementation("io.github.oshai", "kotlin-logging-jvm", "4.0.0-beta-29")
+    implementation("io.github.oshai", "kotlin-logging-jvm", "4.0.0")
     implementation("ch.qos.logback", "logback-classic", "1.4.8")
     implementation(kotlinx("cli-jvm", "0.3.5"))
     implementation(kotlinx("datetime", "0.4.0"))
