6ee42941472accf1e43af088c25d46ae38584691
Kirill M <96192210+httpolar@users.noreply.github.com>
2023-07-22 20:59:41 +0400

    added some reified functions to create name -> instance pairs

--- src/main/kotlin/zaychik/commands/abstracts/AppCommand.kt
+++ src/main/kotlin/zaychik/commands/abstracts/AppCommand.kt
@@ -1,7 +1,13 @@
 package zaychik.commands.abstracts
 
 import dev.kord.core.event.interaction.GuildMessageCommandInteractionCreateEvent
+import kotlin.reflect.full.createInstance
 
 abstract class AppCommand : Command<GuildMessageCommandInteractionCreateEvent>() {
     abstract val name: String
+}
+
+inline fun <reified T : AppCommand> appCommand(): Pair<String, T> {
+    val instance = T::class.createInstance()
+    return instance.name to instance
 }
--- src/main/kotlin/zaychik/commands/abstracts/SlashCommand.kt
+++ src/main/kotlin/zaychik/commands/abstracts/SlashCommand.kt
@@ -2,6 +2,7 @@ package zaychik.commands.abstracts
 
 import dev.kord.core.event.interaction.GuildChatInputCommandInteractionCreateEvent
 import dev.kord.rest.builder.interaction.OptionsBuilder
+import kotlin.reflect.full.createInstance
 
 
 abstract class SlashCommand : Command<GuildChatInputCommandInteractionCreateEvent>() {
@@ -13,4 +14,9 @@ abstract class SlashCommand : Command<GuildChatInputCommandInteractionCreateEven
     val fullName by lazy {
         if (rootName != null) "$rootName $name" else name
     }
+}
+
+inline fun <reified C : SlashCommand> slashCommand(): Pair<String, C> {
+    val instance = C::class.createInstance()
+    return instance.fullName to instance
 }
