2687c29c4a7090f0ff6e3fbb220eaff95fb820f7
Kirill M <96192210+httpolar@users.noreply.github.com>
2023-08-05 17:40:37 +0400

    created gradle catalog libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,11 @@ import org.jetbrains.kotlin.cli.common.arguments.K2JVMCompilerArguments
 
 val mainPath = "zaychik.MainKt"
 
-val koinVersion = "3.4.3"
-val exposedVersion = "0.42.0"
-
-fun kotlinx(name: String, version: String) = "org.jetbrains.kotlinx:kotlinx-$name:$version"
-
 plugins {
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.serialization") version "1.9.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("com.github.ben-manes.versions") version "0.47.0"
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.serialization)
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.dep.versions)
     application
 }
 
@@ -30,21 +25,20 @@ kotlin {
 }
 
 dependencies {
-    implementation("io.github.oshai", "kotlin-logging-jvm", "5.0.2")
-    implementation("ch.qos.logback", "logback-classic", "1.4.8")
-    implementation(kotlinx("cli-jvm", "0.3.5"))
-    implementation(kotlinx("datetime", "0.4.0"))
-    implementation(kotlinx("coroutines-core", "1.7.3"))
-    implementation(kotlinx("serialization-json", "1.5.1"))
-    implementation(kotlinx("collections-immutable", "0.3.5"))
-    implementation("dev.kord", "kord-core", "0.10.0")
-    implementation("com.zaxxer", "HikariCP", "5.0.1")
-    implementation("org.postgresql", "postgresql", "42.6.0")
-    implementation("org.jetbrains.exposed", "exposed-core", exposedVersion)
-    implementation("org.jetbrains.exposed", "exposed-dao", exposedVersion)
-    implementation("org.jetbrains.exposed", "exposed-jdbc", exposedVersion)
-    implementation("org.jetbrains.exposed", "exposed-kotlin-datetime", exposedVersion)
-    implementation("io.insert-koin", "koin-core", koinVersion)
-//    implementation("io.insert-koin", "koin-core-coroutines", koinVersion)
+    implementation(libs.cli)
+    implementation(libs.koin)
+    implementation(libs.coroutines)
+    implementation(libs.datetime)
+    implementation(libs.serialization.json)
+    implementation(libs.immutable.collections)
+    implementation(libs.logger.backend)
+    implementation(libs.logger)
+    implementation(libs.kord)
+    implementation(libs.hikari)
+    implementation(libs.postgresql)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.dao)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.datetime)
 }
 
