de347984a7ce9c37df0a8f873326cdf2251e241f
Kirill M <96192210+httpolar@users.noreply.github.com>
2023-08-06 21:00:08 +0400

    created a waitFor extension for kord

--- src/main/kotlin/zaychik/extensions/Kord.kt
+++ src/main/kotlin/zaychik/extensions/Kord.kt
@@ -1,9 +1,20 @@
 package zaychik.extensions
 
+import dev.kord.core.Kord
 import dev.kord.core.behavior.interaction.ActionInteractionBehavior
 import dev.kord.core.behavior.interaction.response.DeferredMessageInteractionResponseBehavior
 import dev.kord.core.entity.User
+import dev.kord.core.event.Event
 import dev.kord.rest.builder.message.EmbedBuilder
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.timeout
 
 
 suspend fun ActionInteractionBehavior.deferResponse(isEphemeral: Boolean = false): DeferredMessageInteractionResponseBehavior {
@@ -18,3 +29,18 @@ fun embedBuilder(block: EmbedBuilder.() -> Unit = {}): EmbedBuilder {
 }
 
 fun User.displayAvatar() = avatar ?: defaultAvatar
+
+@OptIn(FlowPreview::class)
+suspend inline fun <reified T : Event> Kord.waitFor(
+    timeout: Duration = 90.seconds,
+    capacity: Int = Channel.UNLIMITED,
+    crossinline predicate: suspend (T) -> Boolean
+): Result<T> {
+    return runCatching {
+        events.buffer(capacity)
+            .filterIsInstance<T>()
+            .filter(predicate)
+            .timeout(timeout)
+            .first()
+    }
+}
