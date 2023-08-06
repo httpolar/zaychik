545a67ddaefa5561e30ef34ec3545bd9f46202dd
Kirill M <96192210+httpolar@users.noreply.github.com>
2023-08-06 21:00:11 +0400

    rewritten reaction role creator

--- src/main/kotlin/zaychik/commands/app/CreateReactRoleAppCommand.kt
+++ src/main/kotlin/zaychik/commands/app/CreateReactRoleAppCommand.kt
@@ -1,125 +1,106 @@
 package zaychik.commands.app
 
 import dev.kord.common.entity.Permission
-import dev.kord.core.behavior.interaction.respondEphemeral
+import dev.kord.core.behavior.interaction.response.createEphemeralFollowup
 import dev.kord.core.behavior.interaction.response.edit
-import dev.kord.core.behavior.interaction.updateEphemeralMessage
+import dev.kord.core.behavior.interaction.response.respond
 import dev.kord.core.entity.ReactionEmoji
 import dev.kord.core.event.interaction.GuildMessageCommandInteractionCreateEvent
 import dev.kord.core.event.interaction.GuildSelectMenuInteractionCreateEvent
 import dev.kord.core.event.message.ReactionAddEvent
-import dev.kord.rest.builder.message.create.actionRow
-import dev.kord.rest.request.RestRequestException
+import dev.kord.rest.builder.message.modify.actionRow
 import java.util.UUID
+import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.flow.firstOrNull
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import zaychik.commands.abstracts.AppCommand
 import zaychik.db.entities.ReactRole
-import kotlin.collections.ArrayList
-import kotlin.time.Duration.Companion.seconds
+import zaychik.extensions.waitFor
 
 
 class CreateReactRoleAppCommand : AppCommand() {
     override val name = "Create React Role"
+    private val timeout = 90.seconds
 
     override suspend fun check(event: GuildMessageCommandInteractionCreateEvent): Boolean {
         return event.interaction.user.asMember().getPermissions().values.contains(Permission.ManageRoles)
     }
 
-    @OptIn(FlowPreview::class)
     override suspend fun action(event: GuildMessageCommandInteractionCreateEvent) {
         val kord = event.kord
         val actor = event.interaction.user
         val guild = event.interaction.guild
         val srcMessage = event.interaction.target
 
-        val botsHighestRole = event.interaction.guild
-            .getMember(event.kord.selfId)
-            .roles
-            .toCollection(ArrayList())
-            .maxByOrNull { it.rawPosition }
-
-        if (botsHighestRole == null) {
-            event.interaction.respondEphemeral {
-                content = "I do not have enough permissions to perform this action! " +
-                        "Please, give me a higher role on your server, so I could give roles to people. " +
-                        "pls."
-            }
-            return
-        }
+        val deferredResponse = event.interaction.deferEphemeralResponse()
 
         val roleSelectId = UUID.randomUUID().toString()
-
-        event.interaction.respondEphemeral {
+        val roleSelectResponse = deferredResponse.respond {
             content = "Please, select the role that we're going to be giving out."
             actionRow {
                 roleSelect(customId = roleSelectId)
             }
         }
 
+        val roleSelectResult = kord.waitFor<GuildSelectMenuInteractionCreateEvent>(timeout) {
+            it.interaction.user.id == actor.id && it.interaction.component.customId == roleSelectId
+        }
 
-        val selectMenuEvent = kord.events.buffer(Channel.UNLIMITED)
-            .filterIsInstance<GuildSelectMenuInteractionCreateEvent?>()
-            .filter { e -> e?.interaction?.user?.id == actor.id }
-            .take(1)
-            .timeout(90.seconds)
-            .catch { emit(null) }
-            .firstOrNull()
-            ?: // tell user something fucked up
+        val roleSelectEvent = roleSelectResult.getOrNull()
+
+        if (roleSelectEvent == null) {
+            val errorMessage = when (roleSelectResult.exceptionOrNull()) {
+                is TimeoutCancellationException -> ":warning: Timed out! It took too long for you to select the role, please, retry."
+                else -> ":warning: Something went wrong, please, try again."
+            }
+
+            roleSelectResponse.createEphemeralFollowup { content = errorMessage }
+            roleSelectResponse.delete()
             return
+        }
 
-        val role = selectMenuEvent.interaction.resolvedObjects
-            ?.roles
-            ?.values
-            ?.firstOrNull()
-
+        val deferredSelectResponse = roleSelectEvent.interaction.deferEphemeralResponse()
+        val role = roleSelectEvent.interaction.resolvedObjects?.roles?.values?.first()
         if (role == null) {
-            event.interaction.respondEphemeral {
-                content = ":x: Well, something went wrong and the role you've selected have not been resolved..."
-            }
-            return
-        }
-
-        val askingUserForReactionInteraction = selectMenuEvent.interaction.updateEphemeralMessage {
-            content = "Please, add a reaction to the original message and then this reaction will be used as a trigger."
-        }
-
-        val reactionEmoji = kord.events.buffer(Channel.UNLIMITED)
-            .filterIsInstance<ReactionAddEvent?>()
-            .filter { r -> r?.user?.id == actor.id && r.messageId == srcMessage.id }
-            .take(1)
-            .timeout(90.seconds)
-            .catch { emit(null) }
-            .firstOrNull()
-            ?.emoji
-
-        if (reactionEmoji !is ReactionEmoji.Custom) {
-            askingUserForReactionInteraction.edit {
-                content = ":x: You must provide a custom emoji, unicode emojis are not supported. Please, start again."
-            }
-            return
-        }
-
-        val emoji = guild.emojis.firstOrNull { e -> e.id == reactionEmoji.id }
-        if (emoji == null) {
-            askingUserForReactionInteraction.edit {
-                content = ":x: You must use an emoji that belongs to this server. Please, start again."
+            deferredSelectResponse.respond {
+                content = ":warning: The role you've selected was not present " +
+                        "in event's resolved object, please, try again."
             }
             return
         }
 
 
-        try {
-            srcMessage.addReaction(emoji)
-        } catch (e: RestRequestException) {
-            askingUserForReactionInteraction.edit {
-                content = ":x: I couldn't process the emoji you have used as a reaction. Please, start again."
+        val reactionRequestResponse = deferredSelectResponse.respond {
+            content = "You've picked the ${role.mention} role, now you'll have to add a reaction which would " +
+                    "essentially be giving or taking the role from users who click on the reaction."
+        }
+
+        val reactionResult = kord.waitFor<ReactionAddEvent>(timeout) { e ->
+            val isValidMessage = e.message.id == srcMessage.id
+            val isValidUser = e.user.id == actor.id
+            val isValidEmoji = guild.emojis.firstOrNull { it.id == (e.emoji as? ReactionEmoji.Custom)?.id } != null
+
+            isValidMessage && isValidUser && isValidEmoji
+        }
+
+        val reactionEvent = reactionResult.getOrNull()
+        if (reactionEvent == null) {
+            val errorMessage = when (reactionResult.exceptionOrNull()) {
+                is TimeoutCancellationException -> ":warning: Timed out! It took too long for you to add a reaction, please, retry."
+                else -> ":warning: Something went wrong, please, try again."
             }
+
+            reactionRequestResponse.edit { content = errorMessage }
+            roleSelectResponse.delete()
             return
         }
+
+        // no need to do this safely since we check in the filter of waitFor
+        val reactionEmoji = reactionEvent.emoji as ReactionEmoji.Custom
+
+        srcMessage.addReaction(reactionEmoji)
 
         newSuspendedTransaction(Dispatchers.IO) {
             ReactRole.new {
@@ -133,8 +114,8 @@ class CreateReactRoleAppCommand : AppCommand() {
             }
         }
 
-        askingUserForReactionInteraction.edit {
-            content = "<:verified:1061586076013170719> Done!"
+        reactionRequestResponse.createEphemeralFollowup {
+            content = ":white_check_mark: You have successfully created a reaction role for ${role.mention}"
         }
     }
 }
