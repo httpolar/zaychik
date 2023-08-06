73dd74d486b7aa866a2ea29150837a69cf899c23
Kirill M <96192210+httpolar@users.noreply.github.com>
2023-08-06 16:15:02 +0400

    removed runBlocking hack for kord dependency

--- src/main/kotlin/zaychik/ZaychikModule.kt
+++ src/main/kotlin/zaychik/ZaychikModule.kt
@@ -2,15 +2,11 @@ package zaychik
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
-import dev.kord.core.Kord
-import kotlinx.coroutines.runBlocking
-import org.koin.core.module.dsl.createdAtStart
-import org.koin.core.module.dsl.withOptions
 import org.koin.dsl.module
 
 val zaychikModule = module {
-    single { Config("config.properties") } withOptions {
-        createdAtStart()
+    single {
+        Config("config.properties")
     }
 
     single {
@@ -21,13 +17,6 @@ val zaychikModule = module {
             maximumPoolSize = 10
         }
         HikariDataSource(hikariConfig)
-    }
-
-    single {
-        val config = get<Config>()
-        runBlocking {
-            Kord(config.token)
-        }
     }
 
     single {
